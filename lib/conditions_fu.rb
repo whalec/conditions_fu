a80c89969a9801d1171fc80b285d123753afc80d
Cameron Barrie <camwritescode@gmail.com>
2010-10-18 10:20:21 +1100

    Increment the revision number

--- lib/conditions_fu.rb
+++ lib/conditions_fu.rb
@@ -2,7 +2,7 @@
 module ConditionsFu
 
   # :stopdoc:
-  VERSION = '0.5.0'
+  VERSION = '0.5.1'
   LIBPATH = ::File.expand_path(::File.dirname(__FILE__)) + ::File::SEPARATOR
   PATH = ::File.dirname(LIBPATH) + ::File::SEPARATOR
   # :startdoc:
